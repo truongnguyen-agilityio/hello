0c0f760357a9bffefd234d573258371a4a759d34
Truong Nguyen <truong.nguyenxuan@asnet.com.vn>
2015-07-04 15:43:20 +0700

    add index user view

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   test "Invalid signup information" do
   	get signup_path
   	assert_no_difference 'User.count' do
