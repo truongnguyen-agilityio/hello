c20c817a024f6f6b9ea800a04bd6a46e6b07f5d0
Truong Nguyen <truong.nguyenxuan@asnet.com.vn>
2015-07-04 15:41:09 +0700

    init code

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "Invalid signup information" do
+  	get signup_path
+  	assert_no_difference 'User.count' do
+  		post users_path, user: {name: '', email: 'truong@gmail', password: 'abc', password_confirmation: '123'}
+  	end
+  	assert_template 'users/new'
+  end
+
+  test "Valid signup information" do
+  	get signup_path
+  	assert_difference 'User.count', 1 do
+  		post_via_redirect users_path, user: {name: 'truong', email: 'truong@gmail.com', password: 'password', password_confirmation: 'password'}
+  	end
+  	assert_template 'users/show'
+  end
+end
